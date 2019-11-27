ed9cb5176f5eab314bcfb3c93c8d5a1db1570c34
Alexander Chesser <alex.chesser@gmail.com>
2019-11-27 16:16:31 -0500

    added walking zombie (shambling)

---
+++ ZombieShooter/Assets/Scripts/Components/Enemy/EnemyNavigation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    private Transform playerTransform;
+    private NavMeshPath path;
+    private GameObject cube;
+
+    void Start()
+    {
+        playerTransform = FindObjectOfType<PlayerMovement>().transform;
+        path = new NavMeshPath();
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.GetComponent<Collider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetPosition = playerTransform.position;
+
+        bool foundPath = NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+        if (foundPath) {
+            Vector3 nextDestination = path.corners[1];
+            cube.transform.position = nextDestination;
+            Vector3 directionToTarget = nextDestination - transform.position;
+            Vector3 flatDirection = new Vector3(directionToTarget.x, 0, directionToTarget.z);
+            directionToTarget = Vector3.Normalize(flatDirection);
+            var desiredRotation = Quaternion.LookRotation(directionToTarget);
+            var finalRotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime);
+            transform.rotation = finalRotation;
+        }
+    }
+}
