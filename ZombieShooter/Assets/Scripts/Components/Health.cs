770395c6e83049675a1d0203b356494880ffd0a6
Alexander Chesser <alex.chesser@gmail.com>
2019-11-27 16:27:18 -0500

    added health component

---
+++ ZombieShooter/Assets/Scripts/Components/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 5;
+    private int currentHealth;
+
+    private void OnEnable()
+    {
+        currentHealth = health;
+    }
+
+    public void TakeHit(int amount) {
+        currentHealth -= amount;
+        GetComponentInChildren<Animator>().SetTrigger("Hit");
+    }
+
+
+}
