93022692860e6b63731889732cfa417835ff6f13
Alexander Chesser <alex.chesser@gmail.com>
2019-11-28 12:55:52 -0500

    zombie HIT and DEATH

--- ZombieShooter/Assets/Scripts/Components/Weapon/WeaponRaycast.cs
+++ ZombieShooter/Assets/Scripts/Components/Weapon/WeaponRaycast.cs
@@ -7,6 +7,7 @@ public class WeaponRaycast : WeaponComponent
 {
     [SerializeField] PooledMonoBehaviour decalPrefab;
     [SerializeField] private LayerMask layerMask;
+    [SerializeField] private int damage = 1;
     private RaycastHit hitInfo;
     private float maxDistance = 300f;
 
@@ -14,13 +15,23 @@ public class WeaponRaycast : WeaponComponent
     {
         var ray = weapon.GetAim();
         if (Physics.Raycast(ray, out hitInfo, maxDistance, layerMask)) {
-            SpawnDecal(hitInfo.point, hitInfo.normal);
+            Health health = hitInfo.collider.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeHit(damage);
+            }
+            else
+            {
+                SpawnDecal(hitInfo.point, hitInfo.normal);
+            }
         }
     }
 
     private void SpawnDecal(Vector3 point, Vector3 normal)
     {
-        var decal = decalPrefab.Get<PooledMonoBehaviour>(point, Quaternion.LookRotation(-normal));
-        decal.ReturnToPool(5f);
+        if (decalPrefab != null) {
+            var decal = decalPrefab.Get<PooledMonoBehaviour>(point, Quaternion.LookRotation(-normal));
+            decal.ReturnToPool(5f);
+        }
     }
 }
